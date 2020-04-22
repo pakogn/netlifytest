aa856169cfc0b0c8ef9d17526324e7b47c835a1c
Francisco Daniel Garcia Noriega <pako.gn@hotmail.com>
2020-04-21 20:04:53 -0500

    se corrige referencia a css.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import "./Layout.css"
-const Layout = ({children}) => {
-    return (
-        <main>
-          <Navbar />
-          {children}
-          <Footer />
-        </main>
-    )
+import "./layout.css"
+const Layout = ({ children }) => {
+  return ( <
+    main >
+    <
+    Navbar / > { children } <
+    Footer / >
+    <
+    /main>
+  )
 }
 export default Layout
